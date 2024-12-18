97bd1670e086ed888cf4ba84c7d251e5da0f1662
oyunojino <jinyuno131618@naver.com>
2024-12-18 13:49:01 +0900

    value converter 추가

---
+++ WPF_login-form/ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using WPF_login_form.Pages;
+using WPF_login_form.Word.Core;
+
+namespace WPF_login_form;
+
+public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+{
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        switch ((ApplicationPage)value)
+        {
+            case ApplicationPage.Login:
+                return new LoginPage();
+            default:
+                return null;
+        }
+    }
+
+    public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WPF_login-form/ValueConverters/BaseValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WPF_login_form;
+
+public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+    where T : class, new()
+{
+    #region  Private Members
+    private static T Converter = null;
+    #endregion
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Converter ?? (Converter = new T());
+    }
+
+    public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+    public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+}
