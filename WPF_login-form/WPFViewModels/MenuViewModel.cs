c85ea1cd49e545a94f1efa3dc2d76e5da1a68da8
oyunojino <jinyuno131618@naver.com>
2024-12-19 10:31:19 +0900

    [ Feat ] 창 크기 조절 및 이동 기능 추가

--- WPF_login-form/WPFViewModels/MenuViewModel.cs
+++ WPF_login-form/WPFViewModels/MenuViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System.ComponentModel.Design;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Input;
 using WPF_login_form.Word;
+using WPF_login_form.Word.Core;
 
 namespace WPF_login_form;
 
@@ -11,6 +13,7 @@ public class MenuViewModel
     private Window _window;
     private WindowResizer _windowResizer;
 
+    #region # Commands
     public ICommand Menu1Command { get; set; }
 
     public ICommand MinimizeCommand { get; set; }
@@ -18,6 +21,44 @@ public class MenuViewModel
     public ICommand MaximizeCommand { get; set; }
 
     public ICommand CloseCommand { get; set; }
+    #endregion
+
+
+    public ApplicationPage _currentPage = ApplicationPage.Login;
+
+    #region #window size / move
+
+    // # 창의 제목 표시줄 높이
+    public int TitleHeight { get; set; } = 42;
+
+    // # 창의 외부 여백
+    private Thickness _outerMarginSize = new Thickness(5);
+
+    // # 창이 현재 화면의 모서리에 도킹 되었는지 또는 자유롭게 위치할 수 있는지 나타내는 필드
+    private WindowDockPosition _dockPosition = WindowDockPosition.Undocked;
+
+    // # 창이 최대화 상태이거나 도킹된 경우 true / 그 외에 false
+    public bool Borderless => (_window.WindowState == WindowState.Maximized || _dockPosition != WindowDockPosition.Undocked);
+
+    // # 창 최대화 테두리 두께 0 / 그 외의 경우 4
+    public int ResizeBorder => _window.WindowState == WindowState.Maximized ? 0 : 4;
+
+    // # 창의 외부 여백 크기
+    //  get -> 최대화일 떄, 현재 모니터의 여백 / 그 외일 때, _outerMarginSize
+    //  set -> 외부 여백 크기 설정
+    public Thickness OuterMarginSize
+    {
+        // If it is maximized or docked, no border
+        get => _window.WindowState == WindowState.Maximized ? _windowResizer.CurrentMonitorMargin : (Borderless ? new Thickness(0) : _outerMarginSize);
+        set => _outerMarginSize = value;
+    }
+
+    // # 창 테두리 크기
+    public Thickness ResizeBorderThickness => new Thickness(OuterMarginSize.Left + ResizeBorder,
+                                                                OuterMarginSize.Top + ResizeBorder,
+                                                                OuterMarginSize.Right + ResizeBorder,
+                                                                OuterMarginSize.Bottom + ResizeBorder);
+    #endregion
 
     public MenuViewModel(Window window)
     {
