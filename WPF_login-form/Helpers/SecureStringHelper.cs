d35bca1dd71cbc22bb496f61e3996213aa60a7b2
oyunojino <jinyuno131618@naver.com>
2024-12-19 16:16:57 +0900

    helper 파일 추가

---
+++ WPF_login-form/Helpers/SecureStringHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Security;
+
+namespace WPF_login_form;
+
+public static class SecureStringHelper
+{
+    public static string? Unsecure(this SecureString secureString)
+    {
+        if(secureString == null)
+            return string.Empty;
+
+        var unmanagedString = IntPtr.Zero;
+
+		try
+		{
+            // Unsecures the password
+            unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+            return Marshal.PtrToStringUni(unmanagedString);
+        }
+		finally
+		{
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+		}
+    }
+}
