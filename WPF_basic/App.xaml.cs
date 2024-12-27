47aa0cf7594603b84c1f4c1d396e6d280a7ee0ab
oyunojino <dusrn2xlayjoh@gmail.com>
2024-12-27 16:43:08 +0900

    wpf 기초 정리 first commit

---
+++ WPF_basic/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_basic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
